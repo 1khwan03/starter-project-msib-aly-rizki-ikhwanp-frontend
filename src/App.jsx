3a2338ddbebaab6a26c57f899024a14e73d13993
ikhwan pamuji <ikhwanpamuji@student.unmerpas.ac.id>
2024-04-19 15:34:20 +0700

    Tugas 3

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+import Login from "./Login";
+
+function App() {
+  return (
+    <div
+      className="text-white h-[100vh] flex justify-center bg-cover"
+      style={{ background: "url"("../src/assets/bg.jpg") }}
+    >
+      <Login />
+    </div>
+  );
+}
+
+export default App;
